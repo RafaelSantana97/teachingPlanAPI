f402beb8a8dbec216acf0eb6c317eaf90f295697
Rafael Santana <rafaellauton@outlook.com>
2019-06-15 13:40:04 -0300

    Configuração de perfis

--- planner/src/main/java/edu/planner/TeachingPlanApplication.java
+++ planner/src/main/java/edu/planner/TeachingPlanApplication.java
@@ -1,23 +1,13 @@
 package edu.planner;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import edu.planner.service.DBService;
-
 @SpringBootApplication
 @ComponentScan
 public class TeachingPlanApplication implements CommandLineRunner {
-
-	@Autowired
-	private DBService dbService;
-
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeachingPlanApplication.class, args);
@@ -26,8 +16,5 @@ public class TeachingPlanApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		if ("create".equals(strategy)) {
-			dbService.instantiateTestDatabase();
-		}
 	}
 }
--- planner/src/main/java/edu/planner/config/DevConfig.java
+++ planner/src/main/java/edu/planner/config/DevConfig.java
@@ -3,6 +3,7 @@ package edu.planner.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,9 +17,16 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		dbService.instantiateTestDatabase();
-		return true;
+
+		if ("create".equals(strategy)) {
+			dbService.instantiateTestDatabase();
+			return true;
+		}
+		return false;
 	}
 }
