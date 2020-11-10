c9eacb827b7f0069ad99e4bfd3ce32092b9d6fb2
Rafael Santana <rafaellauton@outlook.com>
2020-11-10 00:36:09 -0300

    subjects updated

--- src/main/java/edu/planner/config/DBService.java
+++ src/main/java/edu/planner/config/DBService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -97,7 +98,7 @@ public class DBService {
         dom11.setAbbreviation(LevelDegree.DOUTOR.getId());
         dom11.setValue1(LevelDegree.DOUTOR.getDescription());
 
-        iDomainRepo.saveAll(Set.of(dom1, dom2, dom3, dom4, dom5, dom6, dom7, dom8, dom9, dom10, dom11));
+        iDomainRepo.saveAll(Arrays.asList(dom1, dom2, dom3, dom4, dom5, dom6, dom7, dom8, dom9, dom10, dom11));
     }
 
     private List<User> createUsers() {
@@ -108,13 +109,14 @@ public class DBService {
         user1.setProfiles(Set.of(Profile.ADMIN.getId(), Profile.COORDINATOR.getId()));
 
         User user2 = new User();
-        user2.setName("André Breda");
-        user2.setEmail("andre.carneiro@facens.br");
+        user2.setName("Talita Berbel");
+        user2.setEmail("talita.berbel@facens.br");
         user2.setLevelDegree(LevelDegree.MESTRE.getId());
         user2.setProfiles(Set.of(Profile.TEACHER.getId()));
 
         User user3 = new User();
-        user3.setName("Glauco Todesco");l
+        user3.setName("Glauco Todesco");
+        user3.setEmail("glauco.todesco@facens.br");
         user3.setLevelDegree(LevelDegree.DOUTOR.getId());
         user3.setProfiles(Set.of(Profile.TEACHER.getId()));
 
@@ -122,7 +124,7 @@ public class DBService {
         user4.setName("Johannes Lochter");
         user4.setEmail("johannes.lochter@facens.br");
         user4.setLevelDegree(LevelDegree.MESTRE.getId());
-        user4.setProfiles(Set.of(Profile.ADMIN.getId(), Profile.COORDINATOR.getId(), Profile.TEACHER.getId()));
+        user4.setProfiles(Set.of(Profile.TEACHER.getId()));
 
         User user5 = new User();
         user5.setName("Jones Artur");
@@ -130,7 +132,7 @@ public class DBService {
         user5.setLevelDegree(LevelDegree.ESPECIALISTA.getId());
         user5.setProfiles(Set.of(Profile.TEACHER.getId()));
 
-        log.info("[bCrypt] Start encoding 5 passwords with {} rounds", Math.round(Math.pow(2,12)));
+        log.info("[bCrypt] Start encoding 5 passwords with {} rounds", Math.round(Math.pow(2, 12)));
         long start = System.currentTimeMillis();
 
         user1.setHashKey(bCryptPasswordEncoder.encode("P@s$_w04d!"));
@@ -140,74 +142,96 @@ public class DBService {
         user5.setHashKey(bCryptPasswordEncoder.encode("*QGAowEdd0rQjoSt"));
 
         long finish = System.currentTimeMillis();
-        log.info("[bCrypt] 5 passwords encoded. Avg time: {} ms", (finish - start)/5.00);
+        log.info("[bCrypt] 5 passwords encoded. Avg time: {} ms", (finish - start) / 5.00);
 
-        return (List<User>) iUserRepo.saveAll(Set.of(user1, user2, user3, user4, user5));
+        return (List<User>) iUserRepo.saveAll(Arrays.asList(user1, user2, user3, user4, user5));
     }
 
     private List<Subject> createSubjects(List<User> users) {
         Subject subj1 = new Subject();
-        subj1.setName("Processamento de Sinais");
-        subj1.setResponsible(users.get(1));
+        subj1.setName("Programação Orientada a Objeto I");
+        subj1.setResponsible(users.get(2));
         subj1.setType(SubjectType.TEORIA);
 
         Subject subj2 = new Subject();
-        subj2.setName("Processamento de Sinais");
-        subj2.setResponsible(users.get(1));
+        subj2.setName("Programação Orientada a Objeto I");
+        subj2.setResponsible(users.get(2));
         subj2.setType(SubjectType.LABORATORIO);
 
         Subject subj3 = new Subject();
-        subj3.setName("Física Experimental");
-        subj3.setResponsible(users.get(1));
+        subj3.setName("Programação Orientada a Objeto II");
+        subj3.setResponsible(users.get(2));
         subj3.setType(SubjectType.LABORATORIO);
 
         Subject subj4 = new Subject();
-        subj4.setName("Química Experimental");
-        subj4.setResponsible(users.get(1));
+        subj4.setName("Programação Orientada a Objeto II");
+        subj4.setResponsible(users.get(2));
         subj4.setType(SubjectType.LABORATORIO);
 
         Subject subj5 = new Subject();
-        subj5.setName("Mecânica de Fluídos");
-        subj5.setResponsible(users.get(1));
-        subj5.setType(SubjectType.LABORATORIO);
+        subj5.setName("Programação Estruturada I");
+        subj5.setResponsible(users.get(0));
+        subj5.setType(SubjectType.TEORIA);
 
         Subject subj6 = new Subject();
-        subj6.setName("Ensaio de Materiais");
-        subj6.setResponsible(users.get(1));
-        subj6.setType(SubjectType.LABORATORIO);
+        subj6.setName("Programação Estruturada II");
+        subj6.setResponsible(users.get(0));
+        subj6.setType(SubjectType.TEORIA);
 
         Subject subj7 = new Subject();
-        subj7.setName("Cálculo I");
+        subj7.setName("Redes de Computadores I");
         subj7.setResponsible(users.get(4));
         subj7.setType(SubjectType.TEORIA);
 
         Subject subj8 = new Subject();
-        subj8.setName("Cálculo II");
+        subj8.setName("Redes de Computadores II");
         subj8.setResponsible(users.get(4));
         subj8.setType(SubjectType.TEORIA);
 
         Subject subj9 = new Subject();
-        subj9.setName("Cálculo III");
+        subj9.setName("Redes de Computadores I");
         subj9.setResponsible(users.get(4));
-        subj9.setType(SubjectType.TEORIA);
+        subj9.setType(SubjectType.LABORATORIO);
 
         Subject subj10 = new Subject();
-        subj10.setName("Cálculo IV");
+        subj10.setName("Redes de Computadores II");
         subj10.setResponsible(users.get(4));
-        subj10.setType(SubjectType.TEORIA);
+        subj10.setType(SubjectType.LABORATORIO);
 
         Subject subj11 = new Subject();
-        subj11.setName("Linguagens Formais e Automatos");
-        subj11.setResponsible(users.get(1));
+        subj11.setName("Inteligência Artificial");
+        subj11.setResponsible(users.get(3));
         subj11.setType(SubjectType.LABORATORIO);
 
         Subject subj12 = new Subject();
-        subj12.setName("Desenvolvimento de Aplicações Web I");
-        subj12.setResponsible(users.get(2));
+        subj12.setName("Inteligência Computacional");
+        subj12.setResponsible(users.get(3));
         subj12.setType(SubjectType.LABORATORIO);
 
+        Subject subj13 = new Subject();
+        subj13.setName("Inteligência Artificial");
+        subj13.setResponsible(users.get(3));
+        subj13.setType(SubjectType.TEORIA);
+
+        Subject subj14 = new Subject();
+        subj14.setName("Inteligência Computacional");
+        subj14.setResponsible(users.get(3));
+        subj14.setType(SubjectType.TEORIA);
+
+        Subject subj15 = new Subject();
+        subj15.setName("Programação Estruturada I");
+        subj15.setResponsible(users.get(1));
+        subj15.setType(SubjectType.LABORATORIO);
+
+        Subject subj16 = new Subject();
+        subj16.setName("Programação Estruturada II");
+        subj16.setResponsible(users.get(1));
+        subj16.setType(SubjectType.LABORATORIO);
+
         return (List<Subject>) iSubjectRepo.saveAll(
-                Set.of(subj1, subj2, subj3, subj4, subj5, subj6, subj7, subj8, subj9, subj10, subj11, subj12));
+                Arrays.asList(subj5, subj15, subj6, subj16, subj1, subj2,
+                        subj7, subj9, subj11, subj13, subj3, subj4, subj8, subj10,
+                        subj12, subj14));
 
     }
 
@@ -228,14 +252,14 @@ public class DBService {
         t2.setSemester(Semester.SEMESTRE_1.getId());
         t2.setTeacher(users.get(1));
 
-        iClassRepo.saveAll(Set.of(t1, t2));
+        iClassRepo.saveAll(Arrays.asList(t1, t2));
     }
 
     private void createCourses(List<User> users, List<Subject> subjects) {
         Course co1 = new Course();
         co1.setCoordinators(Collections.singletonList(users.get(3)));
-        co1.setName("Fundamentos do Cálculo");
-        co1.setSubjects(subjects.subList(6, 10));
+        co1.setName("Engenharia de Computação");
+        co1.setSubjects(subjects);
 
         iCourseRepo.saveAll(Collections.singletonList(co1));
     }
@@ -243,74 +267,74 @@ public class DBService {
     private void createPermissions() {
         Permission p1 = new Permission();
         p1.setResource(Resource.CLASS.getId());
-        p1.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ));
+        p1.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ));
 
         Permission p2 = new Permission();
         p2.setResource(Resource.COURSE.getId());
-        p2.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ));
+        p2.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ));
 
         Permission p3 = new Permission();
         p3.setResource(Resource.SUBJECT.getId());
-        p3.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ));
+        p3.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ));
 
         Permission p4 = new Permission();
         p4.setResource(Resource.USER.getId());
-        p4.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ));
+        p4.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ));
 
-        iPermissionRepo.saveAll(Set.of(p1, p2, p3, p4));
+        iPermissionRepo.saveAll(Arrays.asList(p1, p2, p3, p4));
 
         PermissionBase admin = new PermissionBase();
         admin.setProfile(Profile.ADMIN.getId());
-        admin.getPermissions().addAll(Set.of(p1, p2, p3, p4));
+        admin.getPermissions().addAll(Arrays.asList(p1, p2, p3, p4));
 
         Permission p10 = new Permission();
         p10.setResource(Resource.CLASS.getId());
-        p10.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
+        p10.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
                 PermissionType.UPDATE, PermissionType.DELETE));
 
         Permission p11 = new Permission();
         p11.setResource(Resource.COURSE.getId());
-        p11.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
+        p11.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
                 PermissionType.UPDATE, PermissionType.DELETE));
 
         Permission p12 = new Permission();
         p12.setResource(Resource.SUBJECT.getId());
-        p12.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
+        p12.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
                 PermissionType.UPDATE, PermissionType.DELETE));
 
         Permission p13 = new Permission();
         p13.setResource(Resource.USER.getId());
-        p13.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
+        p13.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.READ, PermissionType.CREATE,
                 PermissionType.UPDATE, PermissionType.DELETE));
 
-        iPermissionRepo.saveAll(Set.of(p10, p11, p12, p13));
+        iPermissionRepo.saveAll(Arrays.asList(p10, p11, p12, p13));
 
         PermissionBase coordinator = new PermissionBase();
         coordinator.setProfile(Profile.COORDINATOR.getId());
-        coordinator.getPermissions().addAll(Set.of(p10, p11, p12, p13));
+        coordinator.getPermissions().addAll(Arrays.asList(p10, p11, p12, p13));
 
         Permission p20 = new Permission();
         p20.setResource(Resource.CLASS.getId());
-        p20.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.CREATE));
+        p20.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.CREATE));
 
         Permission p21 = new Permission();
         p21.setResource(Resource.COURSE.getId());
-        p21.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.CREATE));
+        p21.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.CREATE));
 
         Permission p22 = new Permission();
         p22.setResource(Resource.SUBJECT.getId());
-        p22.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.CREATE));
+        p22.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.CREATE));
 
         Permission p23 = new Permission();
         p23.setResource(Resource.USER.getId());
-        p23.getPermissionTypes().addAll(Set.of(PermissionType.LIST, PermissionType.CREATE));
+        p23.getPermissionTypes().addAll(Arrays.asList(PermissionType.LIST, PermissionType.CREATE));
 
-        iPermissionRepo.saveAll(Set.of(p20, p21, p22, p23));
+        iPermissionRepo.saveAll(Arrays.asList(p20, p21, p22, p23));
 
         PermissionBase teacher = new PermissionBase();
         teacher.setProfile(Profile.TEACHER.getId());
-        teacher.getPermissions().addAll(Set.of(p20, p21, p22, p23));
+        teacher.getPermissions().addAll(Arrays.asList(p20, p21, p22, p23));
 
-        iPermissionBaseRepo.saveAll(Set.of(admin, coordinator, teacher));
+        iPermissionBaseRepo.saveAll(Arrays.asList(admin, coordinator, teacher));
     }
 }
